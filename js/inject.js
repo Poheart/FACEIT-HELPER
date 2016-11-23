80dab346e693829a389d58c9b63cfe6bed03093f
Poheart <pohearthk@gmail.com>
2016-11-24 03:17:39 +0800

    Remove faceitstats.com support

--- js/inject.js
+++ js/inject.js
@@ -749,12 +749,9 @@ var faceitHelper = {
 					var state = $(matchPlayers[j]).find("span").hasClass("helper-stats");
 					if(name == faceitHelper.lobbyStats.data[key].nickname && !state) {
 						// Our targered users for this loop
-							var faceitstats_link = "http://faceitstats.com/profile,name," +  name;
 							var flag_style = $(matchPlayers[j]).find('.match-team-member__details').hasClass('match-team-member__details--right') ? "left:initial;right:0;" : "right:initial;left:0;";
 							$(matchPlayers[j]).find('.match-team-member__details__skill')
 								.after($('<div>', { class: "match-team-member__details__skill player_flag faction"+faceitHelper.lobbyStats.data[key].fraction, style: flag_style }).append($('<img>', { src: faceitHelper.lobbyStats.data[key].country_flag, class: "flag flag--16 skill-icon", onerror: "faceitHelper.imgLoadError(this, 'country')" })));
-							$(matchPlayers[j]).find('.match-team-member__controls--team')
-								.prepend($($('<a>', { target: "_blank", class: "match-team-member__controls__button helper-stats" ,href: faceitstats_link }).append($('<i>', { class:"icon-ic-social-facebook" } ))));
 							$(matchPlayers[j]).find('.match-team-member__details__name > div')
 								.append($('<br>')).append($('<strong>', { text: "ELO: " + faceitHelper.lobbyStats.data[key].elo, class: "text-info" }));
 
